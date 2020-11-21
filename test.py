b186729dd1dc4c6c8e17c033336186b41ea44b2c
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-11-21 21:41:35 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from tensorflow.python.keras import Input
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense
+from tensorflow.python.keras.models import Model
+
+from transformers import TFBertModel
+
+input_layer = Input(shape = (512,), dtype='int64')
+bert = TFBertModel.from_pretrained('bert-base-chinese')(input_layer)
+bert = bert[0]
+dropout = Dropout(0.1)(bert)
+flat = Flatten()(dropout)
+classifier = Dense(units=5)(flat)
+model = Model(inputs=input_layer, outputs=classifier)
+model.summary()
