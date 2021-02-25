5506a6049e4cd8546d68141037793de260447d68
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-02-25 18:57:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from transformers import DistilBertTokenizer, TFDistilBertForMaskedLM
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 model = TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
 input_ids = tf.constant(tokenizer.encode("Hello, my dog is cute"))[None, :]  # Batch size 1
-outputs = model(input_ids, masked_lm_labels=input_ids)
+outputs = model(input_ids)
 prediction_scores = outputs[0]
