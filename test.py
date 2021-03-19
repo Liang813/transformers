891a38f066b49bbd8783c1ddda669d3029f0518e
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-19 09:43:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,4 @@ model = TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
 input_ids = tf.constant(tokenizer.encode("Hello, my dog is cute"))[None, :]  # Batch size 1
 outputs = model(input_ids)
 prediction_scores = outputs[0]
+print("success")
