b2dccb7b9714a38dfec8d63265469dacd0618f73
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-19 09:42:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 from transformers import DistilBertTokenizer, TFDistilBertForMaskedLM
 
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-model = TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
-input_ids = tf.constant(tokenizer.encode("Hello, my dog is cute"))[None, :]  # Batch size 1
-outputs = model(input_ids, masked_lm_labels=input_ids)
-prediction_scores = outputs[0]
+try:
+  tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+  model = TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
+  input_ids = tf.constant(tokenizer.encode("Hello, my dog is cute"))[None, :]  # Batch size 1
+  outputs = model(input_ids, masked_lm_labels=input_ids)
+  prediction_scores = outputs[0]
+except Exception as e:
+  print("TypeError")
+  print(e)
