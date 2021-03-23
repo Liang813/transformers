bf5cb974b45bb6ce078dd735d3b9e43f99e2e741
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-23 09:30:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ from pytorch_transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
 tokenizer.add_special_tokens({"additional_special_tokens": ("@a@", "@b@")})
 tokenizer.all_special_tokens
+print("success")
