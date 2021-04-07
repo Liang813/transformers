8d07199f2d7d9564d53dab0ac1258594a1bc79c4
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-07 16:42:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from tensorflow.python.keras import Input
 from tensorflow.python.keras.layers import Dropout, Flatten, Dense
 from tensorflow.python.keras.models import Model
 from transformers import TFBertModel
+import traceback
 try:
   input_layer = Input(shape = (512,), dtype='int64')
   bert = TFBertModel.from_pretrained('bert-base-chinese')(input_layer)
@@ -12,5 +13,5 @@ try:
   model = Model(inputs=input_layer, outputs=classifier)
   model.summary()
 except Exception as e:
-  print(str(e))
+  traceback.print_exc()
   
