614525266741125e715f1b41660ba0ce152cff7d
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-15 21:48:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from transformers import DistilBertTokenizer, TFDistilBertForMaskedLM
-
+import traceback
 try:
   tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
   model = TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
@@ -8,5 +8,4 @@ try:
   outputs = model(input_ids, masked_lm_labels=input_ids)
   prediction_scores = outputs[0]
 except Exception as e:
-  print("TypeError")
-  print(e)
+  traceback.print_exc(file=open('/script/transformers1418-buggy.txt','w+'))
