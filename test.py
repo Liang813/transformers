c49504afee1ece8449cd07e5ff78e31d794d01ce
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-16 09:17:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from pytorch_transformers import AutoTokenizer
+import traceback
 try:
   tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
   tokenizer.add_special_tokens({"additional_special_tokens": ("@a@", "@b@")})
   tokenizer.all_special_tokens
 except Exception as e:
-  print("TypeError")
-  print(e)
+  traceback.print_exc(file=open('/script/transformers1152-buggy.txt','w+'))
