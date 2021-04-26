a52cf324d8f1281dd483be02b45929f5ebced53e
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-26 20:13:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 from src.transformers import RobertaConfig, TFRobertaForMaskedLM, create_optimizer
-config = RobertaConfig()  
-optimizer,lr = create_optimizer(1e-4,1000000,10000,0.1,1e-6,0.01)
-training_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model = TFRobertaForMaskedLM(config)
-model.compile(optimizer=optimizer, loss=training_loss)
-input = tf.random.uniform(shape=[1,25], maxval=100, dtype=tf.int32)
-hist = model.fit(input, input, epochs=1, steps_per_epoch=1,verbose=0)
+import traceback
+try:
+  config = RobertaConfig()  
+  optimizer,lr = create_optimizer(1e-4,1000000,10000,0.1,1e-6,0.01)
+  training_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+  model = TFRobertaForMaskedLM(config)
+  model.compile(optimizer=optimizer, loss=training_loss)
+  input = tf.random.uniform(shape=[1,25], maxval=100, dtype=tf.int32)
+  hist = model.fit(input, input, epochs=1, steps_per_epoch=1,verbose=0)
+except Exception as e:
+  traceback.print_exc(file=open('transformers6560-buggy.txt','w+'))
