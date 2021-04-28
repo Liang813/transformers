128ca83825602e80dbd6ad5d0ce2a346e175e878
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-28 09:43:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
+# from pytorch_transformers import AutoTokenizer
+# import traceback
+# try:
+#   tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
+#   tokenizer.add_special_tokens({"additional_special_tokens": ("@a@", "@b@")})
+#   tokenizer.all_special_tokens
+# except Exception as e:
+#   traceback.print_exc(file=open('/script/transformers1152-buggy.txt','w+'))
+  
+
 from pytorch_transformers import AutoTokenizer
-import traceback
-try:
-  tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
-  tokenizer.add_special_tokens({"additional_special_tokens": ("@a@", "@b@")})
-  tokenizer.all_special_tokens
-except Exception as e:
-  traceback.print_exc(file=open('/script/transformers1152-buggy.txt','w+'))
+
+tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
+tokenizer.add_special_tokens({"additional_special_tokens": ("@a@", "@b@")})
+tokenizer.all_special_tokens
