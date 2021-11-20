8680b3639ffab1b8b4ceb147946be1d99cbf8ea5
lyk <62755766+Liang813@users.noreply.github.com>
2021-11-20 20:04:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import torch
 from src.transformers import TransfoXLConfig, TransfoXLLMHeadModel
-
-config = TransfoXLConfig()
-lm = TransfoXLLMHeadModel(config)
-test_tensor = torch.LongTensor([[0]])
-print(lm(input_ids=test_tensor, labels=test_tensor)[0])
+import traceback
+try:
+  config = TransfoXLConfig()
+  lm = TransfoXLLMHeadModel(config)
+  test_tensor = torch.LongTensor([[0]])
+  print(lm(input_ids=test_tensor, labels=test_tensor)[0])
+  assert(lm(input_ids=test_tensor, labels=test_tensor)[0].shape == [0,1])
+except Exception as e:
+  traceback.print_exc(file=open('/script/transformers3711-buggy.txt','w+'))
