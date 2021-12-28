41890351404083ad92ee21477063414adaa2ec0d
lyk <62755766+Liang813@users.noreply.github.com>
2021-12-28 10:28:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from tensorflow.python.keras import Input
 from tensorflow.python.keras.layers import Dropout, Flatten, Dense
 from tensorflow.python.keras.models import Model
-
 from transformers import TFBertModel
-
-input_layer = Input(shape = (512,), dtype='int64')
-bert = TFBertModel.from_pretrained('bert-base-chinese')(input_layer)
-bert = bert[0]
-dropout = Dropout(0.1)(bert)
-flat = Flatten()(dropout)
-classifier = Dense(units=5)(flat)
-model = Model(inputs=input_layer, outputs=classifier)
-model.summary()
+import traceback
+try:
+  input_layer = Input(shape = (512,), dtype='int64')
+  bert = TFBertModel.from_pretrained('bert-base-chinese')(input_layer)
+  bert = bert[0]
+  dropout = Dropout(0.1)(bert)
+  flat = Flatten()(dropout)
+  classifier = Dense(units=5)(flat)
+  model = Model(inputs=input_layer, outputs=classifier)
+  model.summary()
+except Exception as e:
+  traceback.print_exc()
 print("success!")
