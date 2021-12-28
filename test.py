fc48669cc42c30285aeca109765d9957f6d2f168
lyk <62755766+Liang813@users.noreply.github.com>
2021-12-28 10:23:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,13 @@ from tensorflow.python.keras.layers import Dropout, Flatten, Dense
 from tensorflow.python.keras.models import Model
 
 from transformers import TFBertModel
-try:
-  input_layer = Input(shape = (512,), dtype='int64')
-  bert = TFBertModel.from_pretrained('bert-base-chinese')(input_layer)
-  bert = bert[0]
-  dropout = Dropout(0.1)(bert)
-  flat = Flatten()(dropout)
-  classifier = Dense(units=5)(flat)
-  model = Model(inputs=input_layer, outputs=classifier)
-  model.summary()
-except Exception as e:
-  print(str(e))
+
+input_layer = Input(shape = (512,), dtype='int64')
+bert = TFBertModel.from_pretrained('bert-base-chinese')(input_layer)
+bert = bert[0]
+dropout = Dropout(0.1)(bert)
+flat = Flatten()(dropout)
+classifier = Dense(units=5)(flat)
+model = Model(inputs=input_layer, outputs=classifier)
+model.summary()
+print("success!")
